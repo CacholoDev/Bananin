cdeb3d308a5922b856ea51f6b0d3b62703c1a625
CacholoDev <adriannoia104@gmail.com>
2024-09-13 03:53:50 +0200

    fixeando Responsive

--- Chon&Tradicion.js
+++ Chon&Tradicion.js
@@ -1,9 +1,36 @@
-const buti = document.getElementById('b1');
 
-buti.addEventListener('click', a);
 
-function a(){
+//H1
+document.addEventListener('DOMContentLoaded', function() {
+    var title = document.querySelector('#main-title');
+    
+    if (title) {
+        
+        title.style.width = '0';
+        title.style.overflow = 'hidden';
+        title.style.transition = 'width 3s ease';  // Agrega una transición suave
+        setTimeout(function() {
+            title.style.width = 'auto';
+        }, 100);  // Espera para permitir que el estilo inicial se aplique
+        // Estilos iniciales para la animación
+        title.style.transition = 'transform 0.6s ease';
+        
+        // Añadir efectos de animación en hover
+        title.addEventListener('mouseover', function() {
+            title.style.transform = 'scale(1.1)';
+        });
 
-    alert('funciona');
+        title.addEventListener('mouseout', function() {
+            title.style.transform = 'scale(1)';
+        });
+    } else {
+        console.error('Elemento con id "main-title" no encontrado.');
+    }
+});
 
-}
+
+
+
+
+
+
